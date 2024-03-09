1e73d8210b90af46673cdbb25887d8028daff29d
Andrii Zelinskyi <zilok45@gmail.com>
2024-03-09 22:20:36 +0200

    add try & except for task_1.py

--- task_1.py
+++ task_1.py
@@ -2,19 +2,23 @@ from datetime import datetime
 
 
 def get_days_from_today(date: str) -> int:
-    date_obj = datetime.strptime(date, "%Y-%m-%d")
-    today_date = datetime.today()
-    date_difference = today_date - date_obj
-    days_difference = date_difference.days
+    try:
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        today_date = datetime.today()
+        date_difference = today_date - date_obj
+        days_difference = date_difference.days
 
-    if days_difference == 1:
-        print(f"Різниця у днях: {days_difference} дeнь")
-    elif 1 < days_difference <= 4:
-        print(f"Різниця у днях: {days_difference} дні")
-    else:
-        print(f"Різниця у днях: {days_difference} днів")
+        if days_difference == 1:
+            print(f"Різниця у днях: {days_difference} дeнь")
+        elif 1 < days_difference <= 4:
+            print(f"Різниця у днях: {days_difference} дні")
+        else:
+            print(f"Різниця у днях: {days_difference} днів")
 
-    return days_difference
+        return days_difference
+    except ValueError:
+        print("Неправильний формат дати. Введіть дату у форматі 'РРРР-ММ-ДД'.")
+        return None
 
 
-get_days_from_today("2024-03-09")
+get_days_from_today("")
